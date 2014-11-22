7859f9cf3384771d20e5702f6ac792dc2a2ba662
Ren Shen <renshen118@gmail.com>
2014-11-22 12:12:28 -0500

    2.2

--- Sim-2/public_html/script/contributions-script.js
+++ Sim-2/public_html/script/contributions-script.js
@@ -355,7 +355,7 @@ function participerDiscussion(title) {
          $("#contr_survived").text(contenu);*/
 
         if(resultat){
-          $("#contr_survived").text( 'et le nombre total de discussion: '+ nbrTotalDiscussion );
+          $("#contr_survived").text('Il lence '+ nbrNouvelleDiscussion + 'discussion, ' + 'et le nombre total de discussion: '+ nbrTotalDiscussion );
         }else{
           $("#contr_survived").text('Pas de discussion pour cet article');
         }
